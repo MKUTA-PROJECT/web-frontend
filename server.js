62c39937eb81125d5936c2790c6e2bffe0ce458d
Simon Machera <cymonmachera1@gmail.com>
2022-04-11 14:59:55 +0300

    ch webhook model

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mims'));
+// add this code
+const whitelist = ['http://localhost:3000']; // list of allow domain
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/mims/index.html'));
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
+
+app.use(express.static('./dist/fonetApp'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   base_url:'https://mkuta.herokuapp.com/'
   // base_url:'http://127.0.0.1:8000/'
 };
